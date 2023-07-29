a7c1f4a8076d6f0a70d08611dff822c63a1fe813
Abhishek L <133156186+Abhishek-L-GitHub@users.noreply.github.com>
2023-07-29 18:51:38 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,6 +1,7 @@
-async function fetchCities() {
-    let url = "https://bus-booking-accio.onrender.com/all/cities"; 
+async function fetchData() {
+    let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"; 
     let response = await fetch(url)
     let data = await response.json();
-    appendCities(data.cities_list); 
+    document.write(data); 
 }
+fetchData();
