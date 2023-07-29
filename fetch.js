8586fc0e6f06d603310164ef08a5ee21588503de
Abhishek L <133156186+Abhishek-L-GitHub@users.noreply.github.com>
2023-07-29 19:09:58 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -2,6 +2,6 @@ async function fetchData() {
     let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"; 
     let response = await fetch(url)
     let data = await response.json();
-    document.write(data); 
+    console.log(data); 
 }
 fetchData();
